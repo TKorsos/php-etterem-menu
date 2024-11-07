8ae8df21e1efb30861bb07748d1018ca36351e3c
Tibor Korsos <tiborkorsos@gmail.com>
2024-11-07 20:47:40 +0100

    recept-generalas-copilot-recept-oldal-sablon-css-fajl

--- views/receptek.php
+++ views/receptek.php
@@ -5,15 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $this->receptek()[1][$_GET["id"]] ?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/main.css">
   </head>
   <body>
     <?php include("nav.php") ?>
     
-    <main class="container-lg">
+    <main class="container-lg py-5">
       <section class="row">
-        <article class="col">
-          <h1><?php echo $this->receptek()[1][$_GET["id"]] ?></h1>
+        <article class="col d-flex flex-column gap-3">
           <div>
+            <h1><?php echo $this->receptek()[1][$_GET["id"]] ?></h1>
             <?php
 
             echo '<div>
@@ -21,6 +22,34 @@
                   </div>';
             
             ?>
+          </div>
+          <div>
+            <h3>Hozzávalók:</h3>
+            <div>
+              <ul class="list-group">
+                <?php
+
+                  foreach($this->receptSeged()[0] as $key => $hozzavalo) {
+                    echo '<li class="list-group-item border-0">'.$hozzavalo.'</li>';
+                  }
+                
+                ?>
+              </ul>
+            </div>
+          </div>
+          <div>
+            <h3>Elkészítés:</h3>
+            <div>
+              <ol class="list-group list-group-numbered">
+                <?php
+
+                foreach($this->receptSeged()[1] as $key => $elkeszites) {
+                  echo '<li class="list-group-item border-0">'.$elkeszites.'.</li>';
+                }
+                
+                ?>
+              </ol>
+            </div>
           </div>
         </article>
       </section>
